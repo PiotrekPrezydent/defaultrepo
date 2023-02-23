5c22777e212d086603c61d091c9117adc89c3dc2
Piotrek <94804932+PiotrekPrezydent@users.noreply.github.com>
2023-02-23 18:58:49 +0100

    Update Class1.cs

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -8,6 +8,10 @@ namespace ClassLibrary1
         {
 
         }
+        public void main()
+        {
+            
+        }
 
         public double Add(double num1, double num2) => num1 + num2;
 
